2606ad0f601308fccf8014d0b4a8af1591621734
Liam Sutton <devliam90@gmail.com>
2021-09-10 14:11:53 +0100

    add eslint + prettier config

---
+++ server/src/routers/tokens.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const rp = require('request-promise');
+
+router.get('/', async (req, res) => {
+  const requestOptions = {
+    method: 'GET',
+    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    qs: {
+      start: '1',
+      limit: '10',
+      convert: 'USD',
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': process.env.API_KEY,
+    },
+    json: true,
+    gzip: true,
+  };
+
+  try {
+    const response = await rp(requestOptions);
+
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
